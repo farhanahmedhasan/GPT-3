38243c3d5bec6171fa8b857e61175bc62ed6b3e3
farhanahmedhasan <achasandjmamunxyz2015@gmail.com>
2021-12-07 20:27:31 +0600

    Added full responsiveness for the header section

--- src/pages/header/Header.jsx
+++ src/pages/header/Header.jsx
@@ -7,7 +7,7 @@ import avatar3 from '../../assests/images/avatar3.png';
 import avatar4 from '../../assests/images/avatar4.png';
 import avatar5 from '../../assests/images/avatar5.png';
 import avatar6 from '../../assests/images/avatar6.png';
-import avatar7 from '../../assests/images/avatar7.png';
+
 import ai from '../../assests/images/ai_hero.png';
 
 const Header = () => {
@@ -20,7 +20,7 @@ const Header = () => {
         <div className='container'>
           <div className='hero py-8 grid grid-cols-12 items-center'>
             {/* Left Side */}
-            <div className='col-span-7'>
+            <div className='col-span-12 lg:col-span-7 mb-12 lg:mb-0'>
               <div className='hero__textBox mb-12'>
                 <h1 className='hero__heading gradient-text mb-12'>Let's Build Something amazing with GPT-3 OpenAI</h1>
                 <p className='hero__para paragraph'>
@@ -30,12 +30,16 @@ const Header = () => {
                 </p>
               </div>
 
-              <div className='hero__contact mb-12'>
-                <input className='formControl w-3/4' type='email' placeholder='Your Email Address' />
+              <div className='hero__contact mb-12 flex'>
+                <input
+                  className='formControl flex-1 xl:flex-none xl:w-3/4'
+                  type='email'
+                  placeholder='Your Email Address'
+                />
                 <button className='btn'>Get Started</button>
               </div>
 
-              <div className='hero__users flex items-center'>
+              <div className='hero__users flex flex-col md:flex-row items-center'>
                 <div className='hero__images flex'>
                   <img src={avatar1} alt='avatar1' />
                   <img src={avatar2} alt='avatar2' />
@@ -44,16 +48,15 @@ const Header = () => {
                   <img src={avatar5} alt='avatar5' />
                   <img src={avatar6} alt='avatar6' />
                   <div>
-                    <img src={avatar7} alt='avatar6' />
-                    <span>1.6k+</span>
+                    <p>1.6k+</p>
                   </div>
                 </div>
-                <p className='ml-4'>1,600 people requested access a visit in last 24 hours</p>
+                <p className='ml-4 mt-4 md:mt-0'>1,600 people requested access a visit in last 24 hours</p>
               </div>
             </div>
 
             {/* Right Side */}
-            <div className='col-span-5'>
+            <div className='col-span-12 lg:col-span-5 place-self-center'>
               <div className='hero__ai'>
                 <img src={ai} alt='ai' />
               </div>
